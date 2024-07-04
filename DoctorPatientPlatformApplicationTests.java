803148819711d61925fae50df51bd03570b26af4
ANAND <152265119+anand8620126@users.noreply.github.com>
2024-07-04 11:28:44 +0530

    Add files via upload
    commit

---
+++ DoctorPatientPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doctor_patient_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorPatientPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
