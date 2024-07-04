9e2f2bdb38efdaf1844c6a6281151665b8d46a86
ANAND <152265119+anand8620126@users.noreply.github.com>
2024-07-04 11:24:04 +0530

    Add files via upload
    commit

---
+++ PatientController.java
@@ -0,0 +1,27 @@
+package com.example.doctor_patient_platform.controller;
+
+
+
+import com.example.doctor_patient_platform.entity.Patient;
+import com.example.doctor_patient_platform.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    @Autowired
+    private PatientService patientService;
+
+    @PostMapping
+    public ResponseEntity<Patient> addPatient(@RequestBody Patient patient) {
+        return ResponseEntity.ok(patientService.addPatient(patient));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removePatient(@PathVariable Long id) {
+        patientService.removePatient(id);
+        return ResponseEntity.noContent().build();
+    }
+}
