10ea9b0f5a755b44bf4372bc2e4375390fc69bbf
ANAND <152265119+anand8620126@users.noreply.github.com>
2024-07-04 11:22:57 +0530

    Add files via upload
    commit changes

---
+++ DoctorPatientPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.doctor_patient_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPatientPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPatientPlatformApplication.class, args);
+	}
+
+}
